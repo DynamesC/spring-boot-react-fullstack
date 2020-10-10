1df4ed68f15c025b8a279dbde480904ea5fcd0fc
Patrick Chen <bravozz0131@gmail.com>
2020-10-10 18:04:28 +0800

    end of 1010

--- src/main/java/com/amigoscode/demo/Hanger/HangerController.java
+++ src/main/java/com/amigoscode/demo/Hanger/HangerController.java
@@ -1,15 +1,25 @@
 package com.amigoscode.demo.Hanger;
 
+import com.amigoscode.demo.DemoApplication;
 import com.amigoscode.demo.Hanger.DAO.HangerProduct;
 import com.amigoscode.demo.Hanger.DAO.HangerTemplate;
 import com.amigoscode.demo.Hanger.Request.RemoveProductsRequest;
 import com.amigoscode.demo.Hanger.Request.UpdateHangerProductRequest;
 import com.amigoscode.demo.Hanger.Response.TotalCountsResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/hanger")
@@ -36,12 +46,6 @@ public class HangerController {
         return hangerService.queryTotalCount();
     }
 
-    @GetMapping(value = "/label/query")
-    @ResponseBody
-    public TotalCountsResponse queryLabels() {
-        return hangerService.queryTotalCount();
-    }
-
     @PostMapping(value = "/product/create")
     Boolean createProduct(@Valid @RequestBody HangerProduct product) {
         return hangerService.createProduct(product);
@@ -62,6 +66,23 @@ public class HangerController {
     @ResponseBody
     public List<HangerTemplate> queryTemplates() {
         return hangerService.queryTemplates();
+    }
+
+    @GetMapping(value = "/label/query")
+    public ResponseEntity<String> queryWoLinkLabelData(@RequestParam("size") int size,
+                                                       @RequestParam("page") int page) {
+        String sign = "80805d794841f1b4";
+        String storeId = "001";
+
+        final String uri = String.format("http://new.wolink.com.cn/api/test/esl/query?store_code=%s&f1=%d&f2=%d&sign=%s", storeId, page, size, sign);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
+
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity<Object>(headers), String.class);
+//        return response;
+        return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
     }
 
 
